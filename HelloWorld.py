d0eb161babe6a5cf03defb051d4d8448af7fb6c5
Mark Orchard <morchard21@gmail.com>
2022-10-31 01:50:26 +0000

    update

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python3.7
+
+# This is a full line comment
+# """ this is a block comment """ and is not free, it takes up memory. 
+#
+
 print("Hello World")
+string = "this is a string"
+print(string)
